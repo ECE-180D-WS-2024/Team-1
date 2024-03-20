00617a311fc8eea5f0f0733aa3ee0e161e7d8b80
matthewfiorella <mfiorell@g.ucla.edu>
2024-03-19 22:21:35 -0700

    Added script to play game

---
+++ PlayGame.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+control_c() {
+    echo "Killing background process"
+    kill $PID
+    exit
+}
+
+trap control_c SIGINT
+
+cd BLE_Module
+python BLE_Writer.py > BLE_Log.txt 2>&1 &
+PID=$!
+cd ..
+python "Bomb Goes Boom.py"
+kill $PID
+exit
