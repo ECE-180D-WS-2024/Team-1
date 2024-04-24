110d15d4573900e45a61a720c07e03239842f6b8
Nicholas Nhien <--unset>
2024-04-24 12:13:21 -0700

    New firmware

--- Arduino-Test-Files/BLE_IMU/BLE_IMU.ino
+++ Arduino-Test-Files/BLE_IMU/BLE_IMU.ino
@@ -26,12 +26,12 @@ BLEService ImuService("a2de8038-5b04-4299-9940-3bfa34d2fc3b"); // Send IMU infor
 BLEByteCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite);
 BLEByteCharacteristic checkCharacteristic("0123", BLERead | BLEWrite);
 // UUIDs were randomly generated. Can always change but need to change corresponding UUID in host file
-BLEIntCharacteristic xAccelCharacteristic("663fdcf8-2126-464d-a6c1-c882f5477fb7", BLERead | BLEWrite);
-BLEIntCharacteristic yAccelCharacteristic("e6ac0344-9aee-49ab-b601-1d26b77cf08c", BLERead | BLEWrite);
-BLEIntCharacteristic zAccelCharacteristic("224ad21b-9c75-4e1c-a3f0-51c0c7d0a9a8", BLERead | BLEWrite);
-BLEIntCharacteristic xAngularVeloCharacteristic("4de63bcd-e713-4e28-9392-3cc1d7efbabc", BLERead | BLEWrite);
-BLEIntCharacteristic yAngularVeloCharacteristic("99c07d47-9018-489b-a937-0d911a61aa69", BLERead | BLEWrite);
-BLEIntCharacteristic zAngularVeloCharacteristic("85c17e72-fb28-4883-9333-479b20fce5a7", BLERead | BLEWrite);
+BLEFloatCharacteristic xAccelCharacteristic("663fdcf8-2126-464d-a6c1-c882f5477fb7", BLERead | BLEWrite);
+BLEFloatCharacteristic yAccelCharacteristic("e6ac0344-9aee-49ab-b601-1d26b77cf08c", BLERead | BLEWrite);
+BLEFloatCharacteristic zAccelCharacteristic("224ad21b-9c75-4e1c-a3f0-51c0c7d0a9a8", BLERead | BLEWrite);
+BLEFloatCharacteristic xAngularVeloCharacteristic("4de63bcd-e713-4e28-9392-3cc1d7efbabc", BLERead | BLEWrite);
+BLEFloatCharacteristic yAngularVeloCharacteristic("99c07d47-9018-489b-a937-0d911a61aa69", BLERead | BLEWrite);
+BLEFloatCharacteristic zAngularVeloCharacteristic("85c17e72-fb28-4883-9333-479b20fce5a7", BLERead | BLEWrite);
 
 const int ledPin = LED_BUILTIN; // pin to use for the LED
 void ImuSetup();
@@ -39,7 +39,6 @@ void BleSetup();
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
 
   // set LED pin to output mode
   pinMode(LED_BUILTIN, OUTPUT);
@@ -92,6 +91,7 @@ void loop() {
         digitalWrite(ledPin, HIGH); // Indicator that data is sending
         IMU.readAcceleration(x, y, z);
         IMU.readGyroscope(rx, ry, rz);
+        /*        
         // Convert to int because float conversion at host causing issues
         tx_x = static_cast<int>(x * 1000);
         tx_y = static_cast<int>(y * 1000);
@@ -99,24 +99,26 @@ void loop() {
         tx_rx = static_cast<int>(rx * 1000);
         tx_ry = static_cast<int>(ry * 1000);
         tx_rz = static_cast<int>(rz * 1000);
+
+        */
         Serial.print("x: ");
-        Serial.println(tx_x);
+        Serial.println(x);
         Serial.print("y: ");
-        Serial.println(tx_y);
+        Serial.println(y);
         Serial.print("z: ");
-        Serial.println(tx_z);
+        Serial.println(z);
         Serial.print("rx: ");
-        Serial.println(tx_rx);
+        Serial.println(rx);
         Serial.print("ry: ");
-        Serial.println(tx_ry);
+        Serial.println(ry);
         Serial.print("rz: ");
-        Serial.println(tx_rz);
-        xAccelCharacteristic.writeValue(tx_x);
-        yAccelCharacteristic.writeValue(tx_y);
-        zAccelCharacteristic.writeValue(tx_z);
-        xAngularVeloCharacteristic.writeValue(tx_rx);
-        yAngularVeloCharacteristic.writeValue(tx_ry);
-        zAngularVeloCharacteristic.writeValue(tx_rz);
+        Serial.println(rz);
+        xAccelCharacteristic.writeValue(x);
+        yAccelCharacteristic.writeValue(y);
+        zAccelCharacteristic.writeValue(z);
+        xAngularVeloCharacteristic.writeValue(rx);
+        yAngularVeloCharacteristic.writeValue(ry);
+        zAngularVeloCharacteristic.writeValue(rz);
         prevUpdate = millis();
       }
     }
@@ -145,7 +147,8 @@ void BleSetup() {
 
   // set advertised local name and service UUID:
   BLE.setLocalName("Nano 33 IoT");
-  BLE.setAdvertisedService(ledService);
+  BLE.setDeviceName("Nano 33 IoT");
+  BLE.setAdvertisedService(ImuService);
 
   // add the characteristic to the service
   ledService.addCharacteristic(switchCharacteristic);
