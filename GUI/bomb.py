14d59cb94a51f7fe2a6e3d33d9180d73f5402e64
Nicholas Nhien <--unset>
2024-05-08 21:39:26 -0700

    Refactor

--- GUI/bomb.py
+++ GUI/bomb.py
@@ -8,21 +8,38 @@ from panda3d.core import TextNode, DirectionalLight, PointLight, Spotlight, Node
 class BombApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        # Setup state
         self.secs_remain = 180
         self.timer_light_on = False
         self.wire_cut = [False] * 7
 
+        # Setup assets
         self.sound_beep = self.loader.loadSfx("assets/sound/beep.mp3")
 
         self.font_ssd = self.loader.loadFont("assets/font/seven_segment.ttf")
         self.font_ssd.setPixelsPerUnit(60)
 
+        # Setup scene
         self.bomb = self.loader.loadModel("assets/model/bomb.bam")
         self.bomb.reparentTo(self.render)
         self.bomb.setPos(0, 5.5, 0)
         self.bomb.setHpr(0, 90, 0)
+
+        self.spotlight = Spotlight("spotlight")
+        self.spotlight_np = self.render.attachNewNode(self.spotlight)
+        self.spotlight_np.setPos(-4, -4, 4)
+        self.spotlight_np.lookAt(self.bomb)
+        self.render.setLight(self.spotlight_np)
+        self.render.setShaderAuto()
+
+        # Setup post-processed components
+        self.__setup_timer()
+        self.__setup_num_displays()
+
+        self.__setup_controls()
+
+    def __setup_timer(self):
         timer_node = self.bomb.find("**/timer")
-        
         self.timer_text_node = TextNode(name="timer_text")
         self.timer_text_node.setText("03:00")
         self.timer_text_node.setFont(self.font_ssd)
@@ -48,27 +65,42 @@ class BombApp(ShowBase):
         self.timer_light_np = self.timer_sphere_np.attachNewNode(timer_light_node)
         self.timer_light_np.setPos(0, 0, 0.5)
 
-        hold_num_np = self.bomb.find("**/hold.num")
-        seq_num_np = self.bomb.find("**/seq.num")
-        ss_num_np = self.bomb.find("**/ss.num")
-        wire_num_np = self.bomb.find("**/wire.num")
-        self.hold_num_text = self.setup_num_display(hold_num_np, 'hold', -0.1, -0.1, 0.5, 90, 270, 90)
-        self.seq_num_text = self.setup_num_display(seq_num_np, 'seq', 0.1, -0.1, -0.5, 0, 90, 180)
-        self.ss_num_text = self.setup_num_display(ss_num_np, 'ss', 0.1, 0.1, -0.5, 0, 90, 270)
-        self.wire_num_text = self.setup_num_display(wire_num_np, 'wire', 0.1, -0.1, 0.5, 180, -90, 90)
-
-        self.spotlight = Spotlight("spotlight")
-        self.spotlight_np = self.render.attachNewNode(self.spotlight)
-        self.spotlight_np.setPos(-4, -4, 4)
-        self.spotlight_np.lookAt(self.bomb)
-        self.render.setLight(self.spotlight_np)
-
-        self.render.setShaderAuto()
-
         self.task_blink_colon = self.taskMgr.add(self.blink_colon, "blink_colon", delay=0.5)
         self.task_decr_time = self.taskMgr.add(self.decrement_time, "decrement_time", delay=1)
         self.task_blink_timer_light = self.taskMgr.add(self.blink_timer_light, "blink_light", delay=1)
 
+    def __setup_num_displays(self):
+        def setup_num_display(disp_np: NodePath, puzzle_name: str, posX, posY, posZ, h, p ,r) -> NodePath:
+            disp_text_bg_node = TextNode(f'{puzzle_name}.disp_bg')
+            disp_text_bg_node.setText("88")
+            disp_text_bg_node.setTextColor(255, 255, 255, 0.3)
+            disp_text_bg_node.setFont(self.font_ssd)
+            disp_text_bg_np = disp_np.attach_new_node(disp_text_bg_node)
+            disp_text_bg_np.setPos(posX, posY, posZ)
+            disp_text_bg_np.setHpr(h, p, r)
+            disp_text_bg_np.setScale(0.125, 0.125, 0.2)
+
+            disp_text_node = TextNode(f'{puzzle_name}.disp')
+            disp_text_node.setText("14")
+            disp_text_node.setTextColor(255, 255, 255, 1)
+            disp_text_node.setFont(self.font_ssd)
+            disp_text_np = disp_np.attach_new_node(disp_text_node)
+            disp_text_np.setPos(posX, posY, posZ + 0.1)
+            disp_text_np.setHpr(h, p, r)
+            disp_text_np.setScale(0.125, 0.125, 0.2)
+
+            return disp_text_node
+        
+        hold_num_np = self.bomb.find("**/hold.num")
+        seq_num_np = self.bomb.find("**/seq.num")
+        ss_num_np = self.bomb.find("**/ss.num")
+        wire_num_np = self.bomb.find("**/wire.num")
+        self.hold_num_text = setup_num_display(hold_num_np, 'hold', -0.1, -0.1, 0.5, 90, 270, 90)
+        self.seq_num_text = setup_num_display(seq_num_np, 'seq', 0.1, -0.1, -0.5, 0, 90, 180)
+        self.ss_num_text = setup_num_display(ss_num_np, 'ss', 0.1, 0.1, -0.5, 0, 90, 270)
+        self.wire_num_text = setup_num_display(wire_num_np, 'wire', 0.1, -0.1, 0.5, 180, -90, 90)
+
+    def __setup_controls(self):
         self.accept('q', self.rotate_bomb_simon_says)
         self.accept('w', self.rotate_bomb_binary)
         self.accept('e', self.rotate_bomb_wires)
@@ -77,28 +109,6 @@ class BombApp(ShowBase):
         self.accept('d', self.rotate_bomb_feet)
         for i in range(7):
             self.accept(str(i), self.cut_wire, extraArgs=[i])
-
-
-    def setup_num_display(self, disp_np: NodePath, puzzle_name: str, posX, posY, posZ, h, p ,r) -> NodePath:
-        disp_text_bg_node = TextNode(f'{puzzle_name}.disp_bg')
-        disp_text_bg_node.setText("88")
-        disp_text_bg_node.setTextColor(255, 255, 255, 0.3)
-        disp_text_bg_node.setFont(self.font_ssd)
-        disp_text_bg_np = disp_np.attach_new_node(disp_text_bg_node)
-        disp_text_bg_np.setPos(posX, posY, posZ)
-        disp_text_bg_np.setHpr(h, p, r)
-        disp_text_bg_np.setScale(0.125, 0.125, 0.2)
-
-        disp_text_node = TextNode(f'{puzzle_name}.disp')
-        disp_text_node.setText("14")
-        disp_text_node.setTextColor(255, 255, 255, 1)
-        disp_text_node.setFont(self.font_ssd)
-        disp_text_np = disp_np.attach_new_node(disp_text_node)
-        disp_text_np.setPos(posX, posY, posZ + 0.1)
-        disp_text_np.setHpr(h, p, r)
-        disp_text_np.setScale(0.125, 0.125, 0.2)
-
-        return disp_text_node
 
     def cut_wire(self, wire_idx):
         if not self.wire_cut[wire_idx]:
