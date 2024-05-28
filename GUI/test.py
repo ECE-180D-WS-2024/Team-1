7f99c654d4f3974ae615959816296c645e11ab82
Nhamakami7 <nicholas_hamakami7@outlook.com>
2024-05-28 13:07:10 -0700

    Wrapped tutorial popup text to stay on page

--- GUI/test.py
+++ GUI/test.py
@@ -2,37 +2,52 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import TextNode
 from direct.gui.DirectButton import DirectButton
 
-
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         
+        # Initialize page index
+        self.currentPage = 0
+        
+        # Create text for each page
+        self.pages = [
+            "Welcome to Bomb Goes Boom! This is a cooperative, 2-player game designed to foster teamwork!",
+            "Before continuing, make sure that the manual player has the bomb defusal manual open.",
+            "Now, we'll go through example games so you, the defusal player, know how all the controls for our game."
+        ]
+        
         # Create a text node
         self.popupTextNode = TextNode('popupTextNode')
-        self.popupTextNode.setText("Popup Text!")
-        self.popupTextNode.setTextColor(1, 0, 0, 1)  # Red color
-        self.popupTextNode.setAlign(TextNode.ACenter)  # Center align
-        self.popupTextNode.setFrameColor(0, 0, 1, 1)  # White border color
-        self.popupTextNode.setFrameLineWidth(2)  # Border width
-        self.popupTextNode.setFrameAsMargin(0.2, 0.2, 0.1, 0.1)
-
-        self.popupTextNode.setCardColor(1, 1, 0.5, 1)
-        self.popupTextNode.setCardAsMargin(0.2, 0.2, 0.1, 0.1)
-        self.popupTextNode.setCardDecal(True)
+        self.popupTextNode.setWordwrap(20)  # Set word wrap to fit text within a certain width
+        self.updateText()
         
         # Create a text node path
         self.popupTextNodePath = aspect2d.attachNewNode(self.popupTextNode)
-        self.popupTextNodePath.setScale(0.1)
-        self.popupTextNodePath.setPos(0, 0, 0.5)  # Position the text above the center
+        self.popupTextNodePath.setScale(0.07)  # Adjusted scale to fit text on screen
+        self.popupTextNodePath.setPos(-0.4, 0, 0)  # Position the text above the center
         
-        # Create a button to close the popup
-        self.closeButton = DirectButton(text=("X", "X", "X", "X"), scale=0.05,
-                                        pos=(0.35, 0, 0.45), command=self.removePopupText)
-        self.closeButton.setTransparency(True)
+        # Create buttons for navigation
+        self.prevButton = DirectButton(text=("Prev", "Prev", "Prev", "Prev"), scale=0.05,
+                                       pos=(-0.35, 0, -0.5), command=self.prevPage)
+        self.prevButton.setTransparency(True)
+        
+        self.nextButton = DirectButton(text=("Next", "Next", "Next", "Next"), scale=0.05,
+                                       pos=(0.35, 0, -0.5), command=self.nextPage)
+        self.nextButton.setTransparency(True)
 
-    def removePopupText(self):
-        self.popupTextNodePath.removeNode()  # Remove the text node path
-        self.closeButton.destroy()  # Destroy the close button
+    def updateText(self):
+        # Set text based on current page
+        self.popupTextNode.setText(self.pages[self.currentPage])
+        
+    def prevPage(self):
+        # Move to previous page
+        self.currentPage = (self.currentPage - 1) % len(self.pages)
+        self.updateText()
+        
+    def nextPage(self):
+        # Move to next page
+        self.currentPage = (self.currentPage + 1) % len(self.pages)
+        self.updateText()
 
 app = MyApp()
 app.run()
